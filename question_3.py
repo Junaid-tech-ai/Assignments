777d2af16b6e677ec7185b67ebd473af1ba4babd
muhammadjunaid <engr.junaid001@gmail.com>
2023-06-20 18:12:11 +0500

    add a new line

--- question_3.py
+++ question_3.py
@@ -22,3 +22,4 @@ percentage_5th_sub=(sub5/total_marks_each_sub)*100
 print("total obtained marks =",total_obtained_marks, "\nPercentage of ist subject =",percentage_ist_sub, "Percentage of 2nd subject =", percentage_2nd_sub, "Percentage of 3rd subject =",percentage_3rd_sub, "Percentage of 4th subject =", percentage_4th_sub, "Percentage of fifth subject =", percentage_5th_sub )
 percentage=(total_obtained_marks/total_marks)*100
 print("Total obtained marks = ",total_obtained_marks, "\nPercentage is = ", percentage, "%")
+
