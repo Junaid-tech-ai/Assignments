f5fdb20d86d7b2f4cee85804b3d962eff32abe92
Muhammad Junaid <137079034+engrjunaid1994@users.noreply.github.com>
2023-06-20 15:56:50 +0500

    Add files via upload

---
+++ question_11.py
@@ -0,0 +1,7 @@
+# Write a program that reads an integer and determines and prints whether it‟s odd or even.
+
+num=int(input("Enter the number: \n"))
+if num%2==0:
+    print(num,'Is even number')
+else:
+    print(num, 'is odd number')
