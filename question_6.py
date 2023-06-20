4eb335280cc5c9b010ed73e8df46e46c93597b2e
muhammadjunaid <engr.junaid001@gmail.com>
2023-06-20 18:20:11 +0500

    deleted file

--- question_6.py
+++
@@ -1,8 +0,0 @@
-# Two numbers (base and exponent) are entered through the keyboard. Write a program to
-# find the value of base raised to the power of exponent.
-
-base_num=int(input('Enter the Base Number: \n'))
-exponent_num=int(input("Enter the Exponent: \n"))
-
-value=base_num**exponent_num
-print("The result is =", value)
