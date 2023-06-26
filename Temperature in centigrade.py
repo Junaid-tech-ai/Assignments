5b59a1242c34244059ff4732f73477a29e6f062a
muhammadjunaid <engr.junaid001@gmail.com>
2023-06-26 20:36:25 +0500

    Initial Committed

--- Temperature in centigrade.py
+++
@@ -1,6 +0,0 @@
-# Temperature of a city in Fahrenheit degrees is input through the keyboard. Write a
-# program to convert this temperature into Centigrade degrees.
-
-temp_f=int(input("Enter the temperature in Fahrenheit: \n"))
-temp_Cel=(5/9)*(temp_f-32)
-print("The Temperature in Centigrade is: ", int(temp_Cel))
--- Zig-Zag pattern with stars.py
+++
@@ -1,8 +0,0 @@
-n=10
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if i%2!=0:
-            print("*  ",end="")
-        else:
-            print("   *", end="")
-    print()
